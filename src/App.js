ddeec16c7979c2c13c0fdeb16dacbdd836c4ddea
BaraVictor <108233528+BaraVictor@users.noreply.github.com>
2022-09-25 22:32:52 +0300

    Adaugat navbar (#1)

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import './App.css';
+import Navbar from './components/navbar/navbar';
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello!</h1>
+      <>
+        <Navbar/>
+      </>
     </div>
   );
 }
