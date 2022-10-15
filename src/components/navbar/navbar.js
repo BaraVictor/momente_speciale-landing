b8ee254b6d771e235329b1bb2df313145d3d14e1
BaraVictor <108233528+BaraVictor@users.noreply.github.com>
2022-10-15 18:01:05 +0300

    Adaugat detalii (#2)

--- src/components/navbar/navbar.js
+++ src/components/navbar/navbar.js
@@ -1,5 +1,6 @@
 import { Component } from "react";
 import "../../style/components/navbar.css";
+import LogoMomenteSpeciale from "../../assets/Logo momente speciale.png"
 
 class navbar extends Component{
   state={clicked : false};
@@ -11,11 +12,12 @@ class navbar extends Component{
     <>
       <nav>
         <div className="navbar-title">
-          Momente Speciale
+          <img src={LogoMomenteSpeciale} style={{width:400, maxWidth:'20%', minWidth:'15%', maxHeight:'20%', minHeight:'15%'}}/>
         </div>
-        <div>
+        <div className="Link">
           <ul id="navbar" className={this.state.clicked ? "#navbar active" : "#navbar"}>
-            <li><a className="active" href="index.html">Acasă</a></li>
+            <li><a href="index.html">Acasă</a></li>
+            <li><a href="index.html">Portofoliu</a></li>
             <li><a href="index.html">Servicii</a></li>
             <li><a href="index.html">Contactează-ne</a></li>
             <li><a href="index.html">Înregistrează-te</a></li>
